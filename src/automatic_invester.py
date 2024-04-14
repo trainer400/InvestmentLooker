b2c8fd067f43534cb64631132559ca09449513e8
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-04-14 22:40:03 +0200

    [AutomaticInvester] Added main method and giant try/except for every non nominal situation

--- src/automatic_invester.py
+++ src/automatic_invester.py
@@ -38,55 +38,73 @@ def save_internal_state(state: InternalState):
     pickle.dump(state, log)
     log.close()
 
-# Delay on startup
-# time.sleep(10)
+
+def main():
+    # Delay on startup
+    time.sleep(10)
+
+    config = read_user_configuration("../invester_config.csv")
+    client = RESTClient(key_file=get_absolute_path(
+        "../" + config.KEY_FILE_NAME))
+
+    # Init the internal state
+    state = load_internal_state()
+
+    # Update loop
+    while True:
+        try:
+            # Update the internal state with fresh data
+            state.timestamp = get_server_timestamp(client)
+            state.current_price = get_current_price(client, config.COIN_NAME)
+            state.current_coin_availability = get_coin_availability(
+                client, config.CURRENCY_NAME)
+            state.current_base_coin_availability = get_coin_availability(
+                client, config.BASE_CURRENCY_NAME)
+            state.considered_avg = get_avg_price(
+                client, config.COIN_NAME, config.AVG_HRS)
+
+            # Make decision
+            decision = make_decision(state, config)
+
+            print(decision)
+
+            # Actuate the decision
+            if decision == Action.BUY:
+                # Buy coins
+                # buy_coin(client, config.COIN_NAME,
+                #          state.current_base_coin_availability)
+
+                # Register the purchase details
+                state.last_buy_price = state.current_price
+                state.last_action = Action.BUY
+
+                # Log the event
+                log_data(get_absolute_path(
+                    "../" + config.LOG_NAME + ".ev"), state)
+
+            elif decision == Action.SELL:
+                # Sell coins
+                # sell_coin(client, config.COIN_NAME, state.current_coin_availability)
+
+                # Register the sell details
+                state.last_action = Action.SELL
+
+                # Log the event
+                log_data(get_absolute_path(
+                    "../" + config.LOG_NAME + ".ev"), state)
+
+            # Save the internal in case of a restart
+            save_internal_state(state)
+
+            # Log the internal state
+            log_data(get_absolute_path("../" + config.LOG_NAME), state)
+            print(f"[{state.timestamp}][INFO] Logged data")
+
+            # Sleep for a minute
+            time.sleep(5)
+        except:
+            print("[ERR] Caught unhandled exception during the process")
 
 
-config = read_user_configuration("../invester_config.csv")
-client = RESTClient(key_file=get_absolute_path("../" + config.KEY_FILE_NAME))
-
-# Init the internal state
-state = load_internal_state()
-
-# Update loop
-while True:
-    # Update the internal state with fresh data
-    state.timestamp = get_server_timestamp(client)
-    state.current_price = get_current_price(client, config.COIN_NAME)
-    state.current_coin_availability = get_coin_availability(
-        client, config.CURRENCY_NAME)
-    state.current_base_coin_availability = get_coin_availability(
-        client, config.BASE_CURRENCY_NAME)
-    state.considered_avg = get_avg_price(
-        client, config.COIN_NAME, config.AVG_HRS)
-
-    # Make decision
-    decision = make_decision(state, config)
-
-    print(decision)
-
-    # Actuate the decision
-    if decision == Action.BUY:
-        # Buy coins
-        # buy_coin(client, config.COIN_NAME,
-        #          state.current_base_coin_availability)
-
-        # Register the purchase details
-        state.last_buy_price = state.current_price
-        state.last_action = Action.BUY
-    elif decision == Action.SELL:
-        # Sell coins
-        # sell_coin(client, config.COIN_NAME, state.current_coin_availability)
-
-        # Register the sell details
-        state.last_action = Action.SELL
-
-    # Save the internal in case of a restart
-    save_internal_state(state)
-
-    # Log the internal state
-    log_data(get_absolute_path("../" + config.LOG_NAME), state)
-    print(f"[{state.timestamp}][INFO] Logged data")
-
-    # Sleep for a minute
-    time.sleep(5)
+if __name__ == "__main__":
+    main()
