3a5b3d6f8e46eb4c4cb699c97436a883fe6abfab
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-04-14 17:00:59 +0200

    [Coinbase] Added sell action

--- src/coinbase_interface.py
+++ src/coinbase_interface.py
@@ -1,5 +1,6 @@
 from coinbase.rest import RESTClient
 from json import dumps
+import random
 
 
 def get_current_price(client: RESTClient, coin_name: str):
@@ -20,3 +21,12 @@ def get_coin_availability(client: RESTClient, currency_name: str):
 def get_server_timestamp(client: RESTClient):
     data = client.get_unix_time()
     return int(data["epochSeconds"])
+
+
+def sell_coin(client: RESTClient, coin_name: str, amount: float) -> tuple:
+    # Creates a random order ID to use a "primary key"
+    order_id = random.randint(0, 1000000000)
+    result = client.market_order_sell(
+        str(order_id), coin_name, f"{amount:.8f}")
+
+    return (bool(result["success"]), "" if result["success"] == "true" else result["error_response"]["error"])
