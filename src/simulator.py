207ba003d3c00f0b44f272c31bddfe4b903438f3
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-04-17 12:40:09 +0200

    [Simulator] Adding get_simulation_data

--- src/simulator.py
+++ src/simulator.py
@@ -1,8 +1,25 @@
 from configuration_reader import *
 from investment_strategy import *
 from coinbase_interface import *
+import time
 
 INITIAL_INVESTMENT = 100
+SIMULATION_DAYS = 10
+
+
+def get_simulation_data(client: RESTClient, coin_name: str, starting_timestamp: int):
+    # A max of 300 samples can be requested
+    current_time = starting_timestamp
+    delta_seconds = 300 * 60
+
+    iterations = SIMULATION_DAYS * 24.0 * 60.0 / 300.0
+    counter = 0
+    while counter < iterations:
+        try:
+            counter += 1
+        except Exception as e:
+            print(f"[ERR] Error retrieving simulation data: {str(e)}")
+            time.sleep(1)
 
 
 def main():
@@ -12,6 +29,10 @@ def main():
     # Create an internal state to use during the simulation
     state = InternalState()
     state.current_base_coin_availability = INITIAL_INVESTMENT
+
+    # Setup the API client
+    client = RESTClient(key_file=get_absolute_path(
+        "../" + config.KEY_FILE_NAME))
 
 
 if __name__ == "__main__":
