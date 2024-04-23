2b034f2404b03e8bbf399eb022109e248268f988
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-04-23 22:48:32 +0200

    [AutomaticInvester] Added equal ripartition of resources

--- src/automatic_invester.py
+++ src/automatic_invester.py
@@ -42,10 +42,49 @@ def save_internal_state(config: UserConfiguration, state: InternalState):
     log.close()
 
 
-def main():
-    # Read the user configuration
-    configs = read_user_configurations("../invester_config.csv")
+def compute_base_coin_availability(client: RESTClient, config: UserConfiguration, configs: list[UserConfiguration], states: dict[str, InternalState]):
+    # In case the last operation is a BUY, the base coin for this instance is 0
+    if states[config.LOG_NAME].last_action == Action.BUY:
+        return 0
 
+    # Sum up all the investments which should have access to the base coin and divide for them
+    counter = 0
+
+    for conf in configs:
+        # The investment should have deposited his part and the base currency type must be the same
+        if states[conf.LOG_NAME].last_action != Action.BUY and conf.BASE_CURRENCY_NAME == config.BASE_CURRENCY_NAME:
+            counter += 1
+
+    # Get the amount of base currency
+    base_currency = get_coin_availability(client, config.BASE_CURRENCY_NAME)
+
+    # Divide equally the amount of base currency available
+    return truncate(base_currency / counter, 2)
+
+
+def compute_coin_availability(client: RESTClient, config: UserConfiguration, configs: list[UserConfiguration], states: dict[str, InternalState]):
+    # In case the last operation is a SELL / SELL_LOSS or NONE, the coin for this instance is 0
+    if states[config.LOG_NAME].last_action == Action.SELL or \
+        states[config.LOG_NAME].last_action == Action.SELL_LOSS or \
+            states[config.LOG_NAME].last_action == Action.NONE:
+        return 0
+
+    # Sum up all the investments which should have access to the investment coin and divide for them
+    counter = 0
+
+    for conf in configs:
+        # The investment should have bought his part and the investment currency type must be the same
+        if states[conf.LOG_NAME].last_action == Action.BUY and conf.CURRENCY_NAME == config.CURRENCY_NAME:
+            counter += 1
+
+    # Get the amount of investment currency
+    currency = get_coin_availability(client, config.CURRENCY_NAME)
+
+    # Divide equally the amount of base currency available
+    return truncate(currency / counter, 8)
+
+
+def main():
     # Delay on startup
     time.sleep(10)
 
@@ -53,6 +92,15 @@ def main():
     new_stdout = open(get_absolute_path("../console.log"), "w")
     sys.stdout = new_stdout
     sys.stderr = new_stdout
+
+    # Read the user configuration
+    configs = read_user_configurations("../invester_config.csv")
+
+    # Load the internal states
+    states = {}
+    for config in configs:
+        state = load_internal_state(config)
+        states[config.LOG_NAME] = state
 
     # Update loop
     while True:
@@ -62,17 +110,17 @@ def main():
                 "../" + config.KEY_FILE_NAME))
 
             # Init the internal state
-            state = load_internal_state(config)
+            state = states[config.LOG_NAME]
 
             try:
                 # Update the internal state with fresh data
                 state.timestamp = get_server_timestamp(client)
                 state.current_price = get_current_price(
                     client, config.COIN_NAME)
-                state.current_coin_availability = get_coin_availability(
-                    client, config.CURRENCY_NAME)
-                state.current_base_coin_availability = get_coin_availability(
-                    client, config.BASE_CURRENCY_NAME)
+                state.current_coin_availability = compute_coin_availability(
+                    client, config, configs, states)
+                state.current_base_coin_availability = compute_base_coin_availability(
+                    client, config, configs, states)
                 state.considered_avg = get_avg_price(
                     client, config.COIN_NAME, config.AVG_HRS, state.timestamp)
 
