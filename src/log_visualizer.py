8bb50ba6bdb0fe6f068169803a95fdcf942fa73c
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-05-20 21:30:27 +0200

    [Visualizer] Added buy and sell lines

--- src/log_visualizer.py
+++ src/log_visualizer.py
@@ -4,7 +4,7 @@ import datetime as dt
 import csv
 import os
 
-LOG_FILE = "../execution_logs/AKT.log"
+LOG_FILE = "../execution_logs/ETC.log"
 
 
 def read_log_file(path: str) -> tuple[list, list, list, list]:
@@ -25,23 +25,34 @@ def read_log_file(path: str) -> tuple[list, list, list, list]:
     data_price = []
     data_avg = []
     data_unix = []
+    data_action = []
     for row in reader:
         data_ts.append(int(row["timestamp"]))
         data_price.append(float(row["current_price"]))
         data_avg.append(float(row["considered_avg"]))
         data_unix.append(dt.datetime.fromtimestamp(int(row["timestamp"])))
+        data_action.append(row["last_action"])
 
     file.close()
-    return (data_ts, data_unix, data_price, data_avg)
+    return (data_ts, data_unix, data_price, data_avg, data_action)
 
 
 def main():
     fig, ax = plt.subplots()
 
-    (data_ts, data_unix, data_price, data_avg) = read_log_file(LOG_FILE)
+    (data_ts, data_unix, data_price, data_avg,
+     data_action) = read_log_file(LOG_FILE)
 
     ax.plot(data_unix, data_price)
     ax.plot(data_unix, data_avg, color="yellow")
+
+    for i in range(1, len(data_action)):
+        if data_action[i] != data_action[i-1]:
+            if "BUY" in data_action[i]:
+                ax.axvline(data_unix[i], color="b", label="BUY")
+            elif "SELL" in data_action[i]:
+                ax.axvline(data_unix[i], color="g", label="SELL")
+
     plt.show()
 
 
